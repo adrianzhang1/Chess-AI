63cc59f00d6b5de2e0421d4ca5dcd03e47af232f
unknown <azazhang7@gmail.com>
2020-11-12 08:27:11 -0700

    added miniMaxMover.py

---
+++ miniMaxMover.py
@@ -0,0 +1,13 @@
+import random
+import chess
+import time
+
+class Player:
+    def __init__(self, board, color, t):
+        pass
+    def eval()
+        board.
+
+    def move(self, board, t):
+        time.sleep(random.random())
+        return random.choice(list(board.legal_moves))
